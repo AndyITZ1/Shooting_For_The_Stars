c86b6d94e78b74daaa9d8c702ac849ab5b714869
Jose Lopez <70908944+jlopez7693@users.noreply.github.com>
2021-06-17 16:59:50 -0400

    Bugfix: paths
    Used os module to properly locate assets.

--- projectSS/game.py
+++ projectSS/game.py
@@ -1,6 +1,6 @@
-import pygame
 import sys
 from projectSS.menu import *
+import os
 
 
 class Game:
@@ -17,30 +17,32 @@ class Game:
 
         self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
 
-        # loading assets
+        # loading assets.
+        # TODO: PLEASE use the below line of code to import assets! Without it, the installation is broken!
+        abs_dir = os.path.dirname(__file__)
         # black-colored quit button (default look) (32x32)
-        self.quit_img = pygame.image.load('assets/exit.png')
+        self.quit_img = pygame.image.load(os.path.join(abs_dir, 'assets/exit.png'))
         # when hovered quit button lights up (32x32)
-        self.quit_light_img = pygame.image.load('assets/exit_light.png')
+        self.quit_light_img = pygame.image.load(os.path.join(abs_dir, 'assets/exit_light.png'))
 
-        self.play_button = pygame.image.load('assets/play.png')
-        self.play_light = pygame.image.load('assets/playlight.png')
+        self.play_button = pygame.image.load(os.path.join(abs_dir, 'assets/play.png'))
+        self.play_light = pygame.image.load(os.path.join(abs_dir, 'assets/playlight.png'))
 
-        self.settings = pygame.image.load('assets/settings.png')
+        self.settings = pygame.image.load(os.path.join(abs_dir, 'assets/settings.png'))
 
         # background 800 x 600 img
-        self.gamebg = pygame.image.load('assets/gamebg.png')
+        self.gamebg = pygame.image.load(os.path.join(abs_dir, 'assets/gamebg.png'))
 
         # font name
-        self.font_loc = 'assets/playmegames.ttf'
+        self.font_loc = os.path.join(abs_dir, 'assets/playmegames.ttf')
 
         # load music, set volume, -1 = plays song infinitely, change it to 0 to play once only
-        pygame.mixer.music.load('assets/8bitmusic.mp3')
+        pygame.mixer.music.load(os.path.join(abs_dir, 'assets/8bitmusic.mp3'))
         pygame.mixer.music.set_volume(0.1)
         pygame.mixer.music.play(-1)
 
         # bgm sound
-        self.sfx_blip = pygame.mixer.Sound('assets/blip.wav')
+        self.sfx_blip = pygame.mixer.Sound(os.path.join(abs_dir, 'assets/blip.wav'))
         self.sfx_blip.set_volume(0.4)
 
         self.mouse = pygame.mouse.get_pos()
