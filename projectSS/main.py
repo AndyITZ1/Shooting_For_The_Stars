713ef0391c1c8b4884faa6312180b1cb427bc104
mkPlasma <fireflame22@gmail.com>
2021-06-21 00:36:41 -0400

    Refactored menus to use GameScreen class, added Button class, added main gameplay screen, various  code tweaks

--- projectSS/main.py
+++ projectSS/main.py
@@ -3,9 +3,6 @@ from projectSS.game import Game
 
 def main():
     game = Game()
-    while True:
-        game.menu.display_menu()
-        game.game_loop()
 
 
 if __name__ == "__main__":
--- projectSS/menus.py
+++ projectSS/menus.py
@@ -1,125 +1,148 @@
-import pygame
+import sys
+from abc import ABC
+from projectSS.gamescreen import GameScreen
+
+
+class Button:
+    def __init__(self, x, y, sprite, sprite_hover, on_click, game):
+        self.x = x
+        self.y = y
+        self.sprite = sprite
+        self.sprite_hover = sprite_hover
+        self.on_click = on_click
+        self.game = game
+        
+        # Set button hitbox dimensions
+        self.width = sprite.get_width()
+        self.height = sprite.get_height()
+        
+        # True if mouse is hovering over button
+        self.mouse_hover = False
+    
+    def update(self):
+        # Update mouse over
+        self.mouse_hover = (self.x <= self.game.mouse_pos[0] < self.x + self.width and
+                            self.y <= self.game.mouse_pos[1] < self.y + self.height)
+        
+        # Invoke on_click if clicked
+        if self.mouse_hover and self.game.mouse_clicked:
+            self.game.assets["sfx_blip"].play()
+            self.on_click()
+    
+    def render(self):
+        self.game.screen.blit(self.sprite_hover if self.mouse_hover else self.sprite, (self.x, self.y))
 
 
 # menu base class for handling multiple menus
-class Menu:
+class Menu(GameScreen, ABC):
     def __init__(self, game):
-        self.game = game
-        self.mid_w, self.mid_h = self.game.WIDTH / 2, self.game.HEIGHT / 2
-        self.run_display = True
+        super().__init__(game)
+        self.center_x, self.center_y = self.game.WIDTH / 2, self.game.HEIGHT / 2
+        self.buttons = []
+    
+    def update_buttons(self):
+        for btn in self.buttons:
+            btn.update()
+    
+    def render_buttons(self):
+        for btn in self.buttons:
+            btn.render()
 
 
 class MainMenu(Menu):
     def __init__(self, game):
-        Menu.__init__(self, game)
-        self.state = 'Home'
-        self.play_x, self.play_y = self.mid_w - 32, self.mid_h
-        self.quit_x, self.quit_y = self.game.WIDTH - 32, self.game.HEIGHT - 32
-        self.setting_x, self.setting_y = 3, 3
+        super().__init__(game)
+        
+        # Add buttons
+        self.btn_play = Button(self.center_x - 32, self.center_y,
+                               self.game.assets["btn_play"], self.game.assets["btn_play_light"],
+                               self.game.show_main_game_screen, self.game)
+        
+        self.btn_quit = Button(self.game.WIDTH - 40, self.game.HEIGHT - 40,
+                               self.game.assets["btn_quit"], self.game.assets["btn_quit_light"],
+                               sys.exit, self.game)
+        
+        self.btn_settings = Button(8, 8,
+                                   self.game.assets["btn_settings"], self.game.assets["btn_settings_light"],
+                                   self.game.show_settings_screen, self.game)
 
-    def display_menu(self):
-        self.run_display = True
-        while self.run_display:
-            self.game.check_events()
-            self.check_input()
-            # draws the background using the image
-            self.game.screen.blit(self.game.assets["main_menu_bg"], (0, 0))
+        self.buttons.append(self.btn_play)
+        self.buttons.append(self.btn_quit)
+        self.buttons.append(self.btn_settings)
 
-            self.game.draw_text('Shooting for the Stars', 40, self.mid_w, self.mid_h-40)
-
-            # the default quit button will be drawn and if hovered over the quit light up button is drawn instead
-            if self.game.WIDTH >= self.game.mouse[0] >= self.quit_x and \
-                    self.game.HEIGHT >= self.game.mouse[1] >= self.quit_y:
-                self.game.screen.blit(self.game.assets["quit_light"], (self.quit_x, self.quit_y))
-            else:
-                self.game.screen.blit(self.game.assets["quit_img"], (self.quit_x, self.quit_y))
-
-            # play button
-            if self.mid_w + 32 >= self.game.mouse[0] >= self.play_x and \
-                    self.mid_h+64 >= self.game.mouse[1] >= self.mid_h:
-                self.game.screen.blit(self.game.assets["play_light"], (self.play_x, self.play_y))
-            else:
-                self.game.screen.blit(self.game.assets["play_button"], (self.play_x, self.play_y))
-
-            # settings button
-            if 35 >= self.game.mouse[0] > self.setting_x and 35 >= self.game.mouse[1] > self.setting_y:
-                self.game.screen.blit(self.game.assets["settings_light"], (self.setting_x, self.setting_y))
-            else:
-                self.game.screen.blit(self.game.assets["settings"], (self.setting_x, self.setting_y))
-
-            pygame.display.update()
-            self.game.reset_state()
-
-    def check_input(self):
-        if self.game.set_pressed:
-            self.game.menu = self.game.options_menu
-            self.run_display = False
+    def update(self):
+        self.update_buttons()
+        
+    def render(self):
+        # Draws the background using the image
+        self.game.screen.blit(self.game.assets["main_menu_bg"], (0, 0))
+        
+        # Title text
+        self.game.draw_text('Shooting for the Stars', 40, self.center_x, self.center_y - 40)
+        
+        self.render_buttons()
 
 
 class SettingsMenu(Menu):
     def __init__(self, game):
-        Menu.__init__(self, game)
-        self.music_level = 5
-        self.sfx_level = 5
+        super().__init__(game)
+        
+        self.btn_exit = Button(8, 8,
+                               self.game.assets["btn_quit"], self.game.assets["btn_quit_light"],
+                               self.game.show_main_menu_screen, self.game)
+        
+        self.btn_music_minus = Button(self.center_x - 64, self.center_y - 64,
+                                      self.game.assets["btn_minus"], self.game.assets["btn_minus_light"],
+                                      self.music_minus, self.game)
+        
+        self.btn_music_plus = Button(self.center_x + 32, self.center_y - 64,
+                                     self.game.assets["btn_plus"], self.game.assets["btn_plus_light"],
+                                     self.music_plus, self.game)
+        
+        self.btn_sfx_minus = Button(self.center_x - 64, self.center_y + 64,
+                                    self.game.assets["btn_minus"], self.game.assets["btn_minus_light"],
+                                    self.sfx_minus, self.game)
+        
+        self.btn_sfx_plus = Button(self.center_x + 32, self.center_y + 64,
+                                   self.game.assets["btn_plus"], self.game.assets["btn_plus_light"],
+                                   self.sfx_plus, self.game)
+        
+        self.buttons.append(self.btn_exit)
+        self.buttons.append(self.btn_music_minus)
+        self.buttons.append(self.btn_music_plus)
+        self.buttons.append(self.btn_sfx_minus)
+        self.buttons.append(self.btn_sfx_plus)
+    
+    def update(self):
+        self.update_buttons()
+    
+    def render(self):
+        # TODO: Why is this a solid color? Shouldn't it just be the same as the main menu bg?
+        # Background color
+        self.game.screen.fill((255, 140, 105))
+        
+        self.game.draw_text('Options', 50, self.center_x, self.center_y - 192)
+        
+        self.game.draw_text('Music Volume', 30, self.center_x, self.center_y - 96)
+        self.game.draw_text(str(int(self.game.setting_music_volume * 10)), 30, self.center_x, self.center_y - 48)
 
-    def display_menu(self):
-        self.run_display = True
-        while self.run_display:
-            self.game.check_events()
-            self.check_input()
-            self.game.screen.fill((255, 140, 105))
-            # back to main menu
-            if 35 >= self.game.mouse[0] > 3 and 35 >= self.game.mouse[1] > 3:
-                self.game.screen.blit(self.game.assets["quit_light"], (3, 3))
-            else:
-                self.game.screen.blit(self.game.assets["quit_img"], (3, 3))
-
-            self.game.draw_text('Options', 50, self.mid_w, self.mid_h*0.4)
-            self.game.draw_text('Music Volume', 30, self.mid_w, self.mid_h*0.6)
-            self.game.draw_text('-', 30, self.mid_w*0.6, self.mid_w*0.6)
-            for i in range(0, self.music_level):
-                pygame.draw.rect(self.game.screen, (0, 0, 0),
-                                 pygame.Rect(self.mid_w*(0.63+i*0.08), self.mid_h*0.75, 12, 20))
-            self.game.draw_text('+', 30, self.mid_w*1.4, self.mid_h*0.8)
-            self.game.draw_text('SFX Volume', 30, self.mid_w, self.mid_h)
-            self.game.draw_text('-', 30, self.mid_w*0.6, self.mid_h*1.2)
-            for i in range(0, self.sfx_level):
-                pygame.draw.rect(self.game.screen, (0, 0, 0),
-                                 pygame.Rect(self.mid_w*(0.63+i*0.08), self.mid_h*1.15, 12, 20))
-            self.game.draw_text('+', 30, self.mid_w*1.4, self.mid_h*1.2)
-            pygame.display.update()
-            self.game.reset_state()
-
-    def check_input(self):
-        blip_flag = False
-        if self.game.LEFT_CLICK:
-            blip_flag = True
-            if self.mid_w*0.58 <= self.game.mouse[0] <= self.mid_w*0.62 and \
-                    self.mid_h*0.78 <= self.game.mouse[1] <= self.mid_w*0.82:
-                if self.music_level > 0:
-                    self.music_level -= 1
-                    pygame.mixer.music.set_volume(self.music_level*0.02)
-            elif self.mid_w*1.38 <= self.game.mouse[0] <= self.mid_w*1.42 and \
-                    self.mid_h*0.78 <= self.game.mouse[1] <= self.mid_w*0.82:
-                if self.music_level < 10:
-                    self.music_level += 1
-                    pygame.mixer.music.set_volume(self.music_level*0.02)
-            elif self.mid_w*0.58 <= self.game.mouse[0] <= self.mid_w*0.62 and \
-                    self.mid_h*1.18 <= self.game.mouse[1] <= self.mid_w*1.22:
-                if self.sfx_level > 0:
-                    self.sfx_level -= 1
-                    self.game.assets["sfx_blip"].set_volume(self.sfx_level*0.08)
-            elif self.mid_w*1.38 <= self.game.mouse[0] <= self.mid_w*1.42 and \
-                    self.mid_h*1.18 <= self.game.mouse[1] <= self.mid_w*1.22:
-                if self.sfx_level < 10:
-                    self.sfx_level += 1
-                    self.game.assets["sfx_blip"].set_volume(self.sfx_level*0.08)
-            else:
-                blip_flag = False
-        if self.game.set_pressed:
-            blip_flag = True
-            self.game.menu = self.game.main_menu
-            self.run_display = False
-
-        if blip_flag:
-            self.game.assets["sfx_blip"].play()
+        self.game.draw_text('SFX Volume', 30, self.center_x, self.center_y + 32)
+        self.game.draw_text(str(int(self.game.setting_sfx_volume * 10)), 30, self.center_x, self.center_y + 80)
+        
+        self.render_buttons()
+    
+    def music_minus(self):
+        self.game.setting_music_volume -= 0.1
+        self.game.update_settings()
+    
+    def music_plus(self):
+        self.game.setting_music_volume += 0.1
+        self.game.update_settings()
+    
+    def sfx_minus(self):
+        self.game.setting_sfx_volume -= 0.1
+        self.game.update_settings()
+    
+    def sfx_plus(self):
+        self.game.setting_sfx_volume += 0.1
+        self.game.update_settings()
