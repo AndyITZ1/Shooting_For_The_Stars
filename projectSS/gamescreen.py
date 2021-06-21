2061a8722b111da647cde7ee1f744d7125aa552d
mkPlasma <fireflame22@gmail.com>
2021-06-20 23:01:35 -0400

    Added game to gamescreen init

--- projectSS/gamescreen.py
+++ projectSS/gamescreen.py
@@ -1,8 +1,11 @@
 from abc import ABC, abstractmethod
 
+
 # Abstract game screen class with update and draw function.
 # Menu screens and the main gameplay screen should inherit from this class.
 class GameScreen(ABC):
+	def __init__(self, game):
+		self.game = game
 	
 	@abstractmethod
 	def update(self):
