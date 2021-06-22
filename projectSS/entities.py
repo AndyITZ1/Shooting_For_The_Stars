7ec2b6fe273f08d8ef1debb8ef049e4cc910abc7
Jose Lopez <70908944+jlopez7693@users.noreply.github.com>
2021-06-22 12:24:19 -0400

    Added gravity and jump mechanics

--- projectSS/entities.py
+++ projectSS/entities.py
@@ -4,69 +4,70 @@ from pygame.locals import *
 
 # For now, player will be represented with red squares.
 class Player(pygame.sprite.Sprite):
-    def __init__(self, game):
+    def __init__(self, game, gameplayscreen):
         super().__init__()
-        self.__game = game
-        self.__surface = pygame.Surface((30, 30))
-        self.__surface.fill((237, 55, 55))
-        self.__rectangle = self.__surface.get_rect()
+        self.game = game
+        self.gameplayscreen = gameplayscreen
+        self.surf = pygame.Surface((30, 30))
+        self.surf.fill((237, 55, 55))
+        self.rect = self.surf.get_rect()
 
-        # Kinematics: Governs the physics of player movement. __vector allows us to make 2D vector variables.
-        self.__vector = pygame.math.Vector2
-        self.__ACCEL = 0.5
-        self.__FRICTION = -0.12     # Can be adjusted to modify player movement
-        self.__position = self.__vector((20, 565))
-        self.__velocity = self.__vector(0, 0)
-        self.__acceleration = self.__vector(0, 0)
+        # Kinematics: Governs the physics of player movement. vec allows us to make 2D vector variables.
+        self.vec = pygame.math.Vector2
+        self.ACC = 0.5
+        self.FRIC = -0.12     # TODO: Change friction value to suit game needs.
+        self.pos = self.vec((20, 565))
+        self.vel = self.vec(0, 0)
+        self.acc = self.vec(0, 0)
 
     # This method allows us to control our player. Heavy use of physics and kinematics.
     def move(self):
         # Reset acceleration, or else player ends up wobbling back and forth
-        self.__acceleration = self.__vector(0, 0)
+        self.acc = self.vec(0, 0.5)     # TODO: Change gravity (y) value to suit game needs.
 
-        # Check if any keyboard keys have been pressed. Modify acceleration accordingly.
+        # Check if any keyboard keys have been pressed. Modify acceleration/velocity accordingly.
         pressed_keys = pygame.key.get_pressed()
         if pressed_keys[K_LEFT]:
-            self.__acceleration.x = -self.__ACCEL
+            self.acc.x = -self.ACC
         if pressed_keys[K_RIGHT]:
-            self.__acceleration.x = self.__ACCEL
+            self.acc.x = self.ACC
+        if pressed_keys[K_SPACE]:
+            self.jump()
 
         # Basic kinematics. They all change based on the acceleration from above.
-        self.__acceleration.x += self.__velocity.x * self.__FRICTION
-        self.__velocity += self.__acceleration
-        self.__position += self.__velocity + 0.5 * self.__acceleration
+        self.acc.x += self.vel.x * self.FRIC
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
 
         # FEATURE: Screen Warping. Player will wrap around screen borders. Can be removed with border acting as barrier.
-        if self.__position.x > self.__game.WIDTH:
-            self.__position.x = 0
-        if self.__position.x < 0:
-            self.__position.x = self.__game.WIDTH
+        if self.pos.x > self.game.WIDTH:
+            self.pos.x = 0
+        if self.pos.x < 0:
+            self.pos.x = self.game.WIDTH
 
         # Update the player rectangle to be at the new position that was calculated above
-        self.__rectangle.midbottom = self.__position
+        self.rect.midbottom = self.pos
 
-    @property
-    def surface(self):
-        return self.__surface
+    # Jump method first check if a player is on a platform before allowing player to jump
+    def jump(self):
+        hits = pygame.sprite.spritecollide(self, self.gameplayscreen.platforms, False)
+        if hits:
+            self.vel.y = -15    # TODO: Change jump value to suit game needs.
 
-    @property
-    def rectangle(self):
-        return self.__rectangle
+    # Player platform collision detection. Check if velocity is greater than 0 to prevent jump cancellation
+    def update(self):
+        hits = pygame.sprite.spritecollide(self, self.gameplayscreen.platforms, False)
+        if self.vel.y > 0:
+            if hits:
+                self.vel.y = 0
+                self.pos.y = hits[0].rect.top + 1   # hits[0] returns the first collision in the list
 
 
 # For now, platforms will be represented with gray rectangles.
 class Platform(pygame.sprite.Sprite):
     def __init__(self, game):
         super().__init__()
-        self.__game = game
-        self.__surface = pygame.Surface((game.WIDTH, 20))
-        self.__surface.fill((211, 211, 211))
-        self.__rectangle = self.__surface.get_rect(center=(game.WIDTH/2, game.HEIGHT - 10))    # center = spawn position
-
-    @property
-    def surface(self):
-        return self.__surface
-
-    @property
-    def rectangle(self):
-        return self.__rectangle
+        self.game = game
+        self.surf = pygame.Surface((game.WIDTH, 20))
+        self.surf.fill((211, 211, 211))
+        self.rect = self.surf.get_rect(center=(game.WIDTH / 2, game.HEIGHT - 10))    # center = spawn position
--- projectSS/gameplayscreen.py
+++ projectSS/gameplayscreen.py
@@ -12,37 +12,42 @@ class GameplayScreen(GameScreen):
     def __init__(self, game):
         super().__init__(game)
         # Create two buttons so player can enter settings or exit game. Add them to our buttons list
-        self.__btn_settings = Button(8, 8,
-                                     self.game.assets["btn_settings"], self.game.assets["btn_settings_light"],
-                                     self.game.show_settings_screen, self.game)
-        self.__btn_quit = Button(self.game.WIDTH - 40, 8,
-                                 self.game.assets["btn_quit"], self.game.assets["btn_quit_light"],
-                                 sys.exit, self.game)
-        self.__buttons = []
-        self.__buttons.append(self.__btn_quit)
-        self.__buttons.append(self.__btn_settings)
+        self.btn_settings = Button(8, 8,
+                                   self.game.assets["btn_settings"], self.game.assets["btn_settings_light"],
+                                   self.game.show_settings_screen, self.game)
+        self.btn_quit = Button(self.game.WIDTH - 40, 8,
+                               self.game.assets["btn_quit"], self.game.assets["btn_quit_light"],
+                               sys.exit, self.game)
+        self.buttons = []
+        self.buttons.append(self.btn_quit)
+        self.buttons.append(self.btn_settings)
 
         # Framerate smoothing objects/variables. Used in update() method.
-        self.__FPS = 60
-        self.__FramePerSecond = pygame.time.Clock()
+        self.FPS = 60
+        self.FramePerSec = pygame.time.Clock()
 
         # Creation of the player and base platform
-        self.__platform1 = Platform(game)
-        self.__player = Player(game)
+        self.PT1 = Platform(game)
+        self.P1 = Player(game, self)
 
         # Creating a list of sprites and adding platform & player in it. Allows easy sprite access later in code.
-        self.__sprites = pygame.sprite.Group()
-        self.__sprites.add(self.__platform1)
-        self.__sprites.add(self.__player)
+        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites.add(self.PT1)
+        self.all_sprites.add(self.P1)
+
+        # Creating a list of platforms. Allows easy platform collision detection. Used in player update() method.
+        self.platforms = pygame.sprite.Group()
+        self.platforms.add(self.PT1)
 
     # All game logic and their changes go in this method
     def update(self):
         # This FramePerSecond clock object limits the FPS, determined by the FPS variable. Smooths gameplay.
-        self.__FramePerSecond.tick(self.__FPS)
+        self.FramePerSec.tick(self.FPS)
         self.update_buttons()
 
         # Update the movement of the player
-        self.__player.move()
+        self.P1.move()
+        self.P1.update()
 
     # All game visuals and their changes go in this method
     def render(self):
@@ -51,13 +56,13 @@ class GameplayScreen(GameScreen):
         self.render_buttons()
 
         # Draw all sprites in our sprite group
-        for entity in self.__sprites:
-            self.game.screen.blit(entity.surface, entity.rectangle)
+        for entity in self.all_sprites:
+            self.game.screen.blit(entity.surf, entity.rect)
 
     def update_buttons(self):
-        for btn in self.__buttons:
+        for btn in self.buttons:
             btn.update()
 
     def render_buttons(self):
-        for btn in self.__buttons:
+        for btn in self.buttons:
             btn.render()
