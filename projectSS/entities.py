942083ef8928bc0384d1daf55ce0bac1ef5648ec
Andytr13 <44204082+Andytr13@users.noreply.github.com>
2021-07-19 11:42:48 -0400

    Update: Sprites for Player and Powerup

--- projectSS/entities.py
+++ projectSS/entities.py
@@ -1,10 +1,12 @@
 import math
 import time
 import random
+import os.path
 import pygame
 from pygame.locals import *
 from pygame.math import Vector2
 from abc import ABC, abstractmethod
+from projectSS.spritesheet import *
 
 
 # Abstract entity class
@@ -19,6 +21,7 @@ class Entity(pygame.sprite.Sprite, ABC):
         self.rect_render = self.surf.get_rect()
 
         self.pos = Vector2(0, 0)
+        self.last_pos = self.pos
 
     def update(self):
         if self.pos.y > self.gameplay_screen.despawn_y:
@@ -40,9 +43,25 @@ class Player(Entity):
     def __init__(self, gameplay_screen):
         super().__init__(gameplay_screen)
 
+        # This can be refactor if needed, but uses the Spritesheet class to load and pick out images
+        abs_dir = os.path.dirname(__file__)
+        self.player_spritesheet = Spritesheet(os.path.join(abs_dir, 'assets/player_spritesheetr.png'))
+        # Variables for animation/switching of sprites
+        self.play_walk = False
+        self.play_jump = False
+        self.current_frame = 0
+        self.last_update = 0
+        # Variable to detect what was the last direction the player moved (Either left or right) F = left, T = right
+        self.last_direction = False
+        # To reduce clutter in init all frames images are put in load_images()
+        self.load_images()
+        # surf used to be the rectangle/square player and now is changed to hold the first frame of idle sprite
+        # surf variable will be changed often for each different frame of animation
+        self.surf = self.idle_walk_frames_r[0]
+
         # Rendering surface
-        self.surf = pygame.Surface((30, 30))
-        self.surf.fill((237, 55, 55))
+        # self.surf = pygame.Surface((30, 30))
+        # self.surf.fill((237, 55, 55))
 
         # Velocity and acceleration variables
         self.vel = Vector2(0, 0)
@@ -64,14 +83,38 @@ class Player(Entity):
 
         # Power-up effects
         self.boosted = False
+        self.immune = False
 
         # Rhythm gameplay.
         self.start_time = time.time() + 0.01
 
+    def load_images(self):
+        self.idle_walk_frames_l = [self.player_spritesheet.get_image(3, 3, 58, 58),
+                                   self.player_spritesheet.get_image(2, 136, 56, 60)]
+        self.idle_walk_frames_r = [self.player_spritesheet.get_image(137, 3, 58, 58),
+                                   self.player_spritesheet.get_image(140, 136, 56, 60)]
+        self.jump_frames = [self.player_spritesheet.get_image(2, 70, 64, 60),
+                            self.player_spritesheet.get_image(134, 70, 64, 60)]
+        # IL, IR, WL, WR, JL, JR
+        self.rhythm_jump_frames = [self.player_spritesheet.get_image(68, 3, 58, 58),
+                                   self.player_spritesheet.get_image(204, 3, 58, 58),
+                                   self.player_spritesheet.get_image(67, 136, 56, 60),
+                                   self.player_spritesheet.get_image(207, 136, 56, 60),
+                                   self.player_spritesheet.get_image(66, 69, 64, 60),
+                                   self.player_spritesheet.get_image(200, 70, 64, 60)]
+        self.player_boost_frames = [self.player_spritesheet.get_image(0, 198, 66, 66),
+                                    self.player_spritesheet.get_image(66, 198, 66, 66)]
+        self.invinc_idle_walk_frames_l = [self.player_spritesheet.get_image(0, 264, 66, 66),
+                                          self.player_spritesheet.get_image(130, 198, 66, 66)]
+        self.invinc_idle_walk_frames_r = [self.player_spritesheet.get_image(66, 264, 66, 66),
+                                          self.player_spritesheet.get_image(196, 198, 66, 66)]
+        self.invinc_jump_frames = [self.player_spritesheet.get_image(130, 264, 66, 66),
+                                   self.player_spritesheet.get_image(196, 264, 66, 66)]
+
     # Reset player variables when starting gameplay
     def reset(self):
         self.pos.x = self.game.WIDTH / 2
-        self.pos.y = -25
+        self.pos.y = -64
 
         self.vel.x = 0
         self.vel.y = 0
@@ -84,6 +127,7 @@ class Player(Entity):
         self.on_ground = False
         self.jumping = False
         self.jumped = False
+        self.immune = False
 
     # This method allows us to control our player. Heavy use of physics and kinematics.
     def move(self):
@@ -98,17 +142,23 @@ class Player(Entity):
         # Check if any keyboard keys have been pressed. Modify acceleration/velocity accordingly.
         pressed_keys = pygame.key.get_pressed()
         if pressed_keys[K_LEFT]:
+            self.last_direction = False
+            self.play_walk = True
             self.acc.x = -self.ACCELERATION
         elif pressed_keys[K_RIGHT]:
+            self.last_direction = True
+            self.play_walk = True
             self.acc.x = self.ACCELERATION
 
         # Player is holding space key? Jump until max jump height is reached. Space key is let go? Stop jump.
         if pressed_keys[K_SPACE]:
             self.jump()
             self.jumping = True
+            self.play_jump = True
         else:
             self.cancel_jump()
             self.jumping = False
+            self.play_jump = False
 
         # Apply friction
         if self.on_ground:
@@ -145,13 +195,53 @@ class Player(Entity):
                     self.vel.y = -20
                     self.vel.x *= 2
                     self.game.assets["sfx_blip"].play()
+                    if self.last_direction:
+                        self.surf = self.rhythm_jump_frames[5]
+                    else:
+                        self.surf = self.rhythm_jump_frames[4]
                 else:
                     self.vel.y = -15
+                    if self.last_direction:
+                        self.surf = self.jump_frames[1]
+                    else:
+                        self.surf = self.jump_frames[0]
 
     def cancel_jump(self):
         if self.jumping:
             if self.vel.y < -5:
                 self.vel.y = -5
+
+    def animate_walk(self, direction_frames):
+        now = pygame.time.get_ticks()
+        if self.play_walk:
+            if now - self.last_update > 150:
+                self.last_update = now
+                self.current_frame = (self.current_frame + 1) % len(direction_frames)
+                self.surf = direction_frames[self.current_frame]
+
+    def animate(self, left_frames, right_frames):
+        if not self.play_walk and not self.play_jump:
+            if self.last_direction:
+                self.surf = right_frames[0]
+            else:
+                self.surf = left_frames[0]
+        elif self.play_walk:
+            if self.last_direction:
+                self.animate_walk(right_frames)
+            else:
+                self.animate_walk(left_frames)
+
+    def rhythm_jump_animate(self):
+        if not self.play_walk and not self.play_jump:
+            if self.last_direction:
+                self.surf = self.rhythm_jump_frames[1]
+            else:
+                self.surf = self.rhythm_jump_frames[0]
+        elif self.play_walk:
+            if self.last_direction:
+                self.surf = self.rhythm_jump_frames[3]
+            else:
+                self.surf = self.rhythm_jump_frames[2]
 
     # Player platform collision detection & rhythm restart
     def update(self):
@@ -174,7 +264,7 @@ class Player(Entity):
                 # Place the player on the platform if they are high enough (w/ vel forgiveness) and falling
                 if self.vel.y > 0 and self.rect.bottom < p.rect.centery + self.vel.y:
                     # Set player to be slightly inside platform, otherwise they will jitter
-                    self.pos.y = p.rect.top - 14.9
+                    self.pos.y = p.rect.top - ((self.rect.height / 2) - 0.1)
                     self.vel.y = 0
                     self.on_ground = True
 
@@ -185,16 +275,22 @@ class Player(Entity):
         self.update_rect()
 
         if self.boosted:
-            self.surf.fill((255,165,0))
+            if self.last_direction:
+                self.surf = self.player_boost_frames[1]
+            else:
+                self.surf = self.player_boost_frames[0]
+        elif self.immune:
+            self.animate(self.invinc_idle_walk_frames_l, self.invinc_idle_walk_frames_r)
         else:
             cur_time = time.time()
-            # turn green within a 0.3 interval of the bpm start time
+            # changes to rhythm jump animation within a 0.3 interval of the bpm start time
             if cur_time - self.start_time <= 0.15:
-                self.surf.fill((0, 255, 0))
-            elif cur_time - self.start_time >= (1/83)*60-0.15:
-                self.surf.fill((0, 255, 0))
+                self.rhythm_jump_animate()
+            elif cur_time - self.start_time >= (1 / 83) * 60 - 0.15:
+                self.rhythm_jump_animate()
             else:
-                self.surf.fill((237, 55, 55))
+                self.animate(self.idle_walk_frames_l, self.idle_walk_frames_r)
+
         # 83 BPM or 0.7229 seconds-per-beat: (1 / 83 bpm) * 60 to get exact seconds
         if (time.time() - self.start_time) > (1/83)*60:
             self.start_time = time.time()
@@ -204,15 +300,24 @@ class Player(Entity):
         for p in pows_collisions:
             if p.type == 'boost':
                 self.boosted = True
+            elif p.type == 'invincible':
+                self.immune = True
 
         # Check if player hits enemy
         enemy_collisions = pygame.sprite.spritecollide(self, self.gameplay_screen.enemies, True)
         if enemy_collisions:
-            self.game.assets["sfx_hit"].play()
-            self.hit = True
-            for e in enemy_collisions:
-                e.kill()
+            # If player is in IMMUNE STATE, will lose immunity after hitting 1 enemy
+            if self.immune:
+                self.immune = False
+            else:
+                self.game.assets["sfx_hit"].play()
+                self.hit = True
+                for e in enemy_collisions:
+                    e.kill()
 
+        # Walking to Idle Animation transition
+        if self.last_pos.x + 0.005 >= self.pos.x >= self.last_pos.x - 0.005 and self.on_ground:
+            self.play_walk = False
 
 # For now, platforms will be represented with gray rectangles.
 class Platform(Entity):
@@ -254,11 +359,25 @@ class Enemy(Entity):
 class Powerup(Entity):
     def __init__(self, gameplay_screen, x, y):
         super().__init__(gameplay_screen, gameplay_screen.entities, gameplay_screen.powerups)
-        self.type = random.choice(['boost'])
-        self.surf = pygame.Surface((15, 15))
-        self.surf.fill((200, 100, 200))
+        self.type = random.choice(['boost', 'invincible'])
+        if self.type == 'boost':
+            self.power_frames = gameplay_screen.j_boost_frames
+        else:
+            self.power_frames = gameplay_screen.invinc_frames
+        self.surf = self.power_frames[0]
+        self.last_update = 0
+        self.current_frame = 0
 
         self.pos.x = x
-        self.pos.y = y
+        self.pos.y = y - 20
 
         self.update_rect()
+
+    def update(self):
+        now = pygame.time.get_ticks()
+        if now - self.last_update > 150:
+            self.last_update = now
+            self.current_frame = (self.current_frame + 1) % len(self.power_frames)
+            self.surf = self.power_frames[self.current_frame]
+        self.update_rect()
+
