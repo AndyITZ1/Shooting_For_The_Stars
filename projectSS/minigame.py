b66a4bb408edeb373779c2d605b48b36e67bdc76
Jose Lopez <70908944+jlopez7693@users.noreply.github.com>
2021-07-19 13:26:43 -0400

    Minigame rings will not collide with each other

--- projectSS/minigame.py
+++ projectSS/minigame.py
@@ -1,16 +1,20 @@
 import os
 import random
+from math import sqrt
 import pygame
 from projectSS.gamescreen import GameScreen
 
 
 class ClickedTooLate(Exception):
+
     def __init__(self, message="Didn't click the minigame button in time"):
         self.message = message
 
 
-class Ring:
+class Ring(pygame.sprite.Sprite):
+
     def __init__(self, x, y, sprite, game):
+        super().__init__()
         self.x = x
         self.y = y
         self.sprite = sprite
@@ -18,30 +22,38 @@ class Ring:
 
         self.width = sprite.get_width()
         self.height = sprite.get_height()
-        self.radius = 200
+        self.outer_radius = 200  # Outer ring starting outer_radius.
+        self.inner_radius = self.width / 2  # Inner sprite radius. Used in collision detection
 
-        self.mouse_hover = False
+        self.mouse_hover = False  # Boolean used in mouse detection.
 
     def update(self):
-        # Update mouse over
+        # Checks if mouse is hovering over the sprite using the sprite's position and size.
         self.mouse_hover = (self.x <= self.game.mouse_pos[0] < self.x + self.width and
                             self.y <= self.game.mouse_pos[1] < self.y + self.height)
 
-        # Invoke on_click if clicked
-        if self.mouse_hover and self.game.mouse_clicked and 52 <= self.radius <= 67:
+        if self.mouse_hover and self.game.mouse_clicked and 52 <= self.outer_radius <= 67:
             self.game.assets["sfx_blip"].play()
             return True
         else:
-            if self.radius < 52:
+            if self.outer_radius < 52:
                 raise ClickedTooLate
-            self.radius -= 1.75
+            self.outer_radius -= 1.75
 
     def render(self):
         self.game.screen.blit(self.sprite, (self.x, self.y))
-        pygame.draw.circle(self.game.screen, (134, 144, 250), (self.x + 64, self.y + 64), self.radius, width=3)
+        pygame.draw.circle(self.game.screen, (134, 144, 250), (self.x + 64, self.y + 64), self.outer_radius, width=3)
+
+    def center(self):
+        return self.x + self.width, self.y + self.height
+
+    @property
+    def radius(self):
+        return self.inner_radius
 
 
 class MinigameScreen(GameScreen):
+
     def __init__(self, game):
         super().__init__(game)
         self.game = game
@@ -52,6 +64,8 @@ class MinigameScreen(GameScreen):
         self.counter_str = str(self.counter)
         self.active_circles = []
         self.dormant_circles = []
+        self.max_spawn_time = 1.8  # TODO: Make boss battles harder per iteration
+        self.min_spawn_time = 0.7
 
     def on_show(self):
         pygame.mixer.music.load(os.path.join(os.path.dirname(__file__), 'assets/minigame_bgm.mp3'))
@@ -59,11 +73,23 @@ class MinigameScreen(GameScreen):
 
         self.dormant_circles.clear()
         self.active_circles.clear()
-        for _ in range(0, 15):
-            self.dormant_circles.append(
-                Ring(random.randrange(self.game.WIDTH - 128), random.randrange(self.game.HEIGHT - 128),
-                     self.game.assets["circle"], self.game),
-            )
+
+        num_rings = 15
+        while len(self.dormant_circles) < num_rings:
+            new_ring = Ring(random.randrange(self.game.WIDTH - 128), random.randrange(self.game.HEIGHT - 128),
+                            self.game.assets["circle"], self.game)
+            if not self.dormant_circles:
+                self.dormant_circles.append(new_ring)
+            else:
+                collided = False
+                for neighbor in self.dormant_circles:
+                    new_x, new_y = new_ring.center()
+                    neighbor_x, neighbor_y = neighbor.center()
+                    distance = sqrt(((new_x - neighbor_x) ** 2) + ((new_y - neighbor_y) ** 2))
+                    if distance < new_ring.radius * 2:
+                        collided = True
+                if not collided:
+                    self.dormant_circles.append(new_ring)
 
         self.counting_down = True
         self.counter = 7
@@ -85,7 +111,7 @@ class MinigameScreen(GameScreen):
         else:
             if self.dormant_circles:
                 seconds = (pygame.time.get_ticks() - self.ring_ticks) / 1000
-                if seconds >= 2.5 or seconds >= random.uniform(0.6, 1.8):
+                if seconds >= random.uniform(self.min_spawn_time, self.max_spawn_time):
                     self.active_circles.append(self.dormant_circles.pop(0))
                     self.ring_ticks = pygame.time.get_ticks()
 
